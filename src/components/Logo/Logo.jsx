ee8b174236fdf17dde45551abeac49af5a9591fe
kostyaslob <kostyaslob@gmail.com>
2025-09-14 01:21:11 +0300

    add svg logo to the Header

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,6 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function Logo() {
   return (
-    <Link to="/">RentalCar</Link>    
+    <Link to="/">
+      <svg width="104" height="16">
+        <use href="/sprite.svg#icon-rentalcar"></use>
+      </svg>
+    </Link>
   );
 }
