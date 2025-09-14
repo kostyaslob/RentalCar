07cf45d45f5c653c06ac99189bfcca7e92645971
kostyaslob <kostyaslob@gmail.com>
2025-09-14 17:44:37 +0300

    customization of Select in SearchBar

--- src/components/Catalog/SearchBar/SearchBar.jsx
+++ src/components/Catalog/SearchBar/SearchBar.jsx
@@ -1,11 +1,19 @@
 import css from "./SearchBar.module.css";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Select from "react-select";
+import Select, { components } from "react-select";
 import { fetchBrands } from "../../../redux/operations";
 import { setFilters } from "../../../redux/slice";
 import { Loader } from "../../Loader/Loader";
 import toast from "react-hot-toast";
+
+const DropdownIndicator = (props) => (
+  <components.DropdownIndicator {...props}>
+    <svg width="16" height="16">
+      <use href="/sprite.svg#icon-chevron-down"></use>
+    </svg>
+  </components.DropdownIndicator>
+);
 
 export default function SearchBar() {
   const dispatch = useDispatch();
@@ -38,9 +46,18 @@ export default function SearchBar() {
     label: brand,
   }));
 
+  const formatNumber = (value) => {
+    if (!value) return "";
+    const numericValue = value.toString().replace(/\D/g, "");
+    return numericValue.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
     setSearchTriggered(true);
+
+    const cleanNumber = (value) => (value ? value.replace(/\s/g, "") : "");
+
     dispatch(
       setFilters({
         brand:
@@ -48,8 +65,8 @@ export default function SearchBar() {
             ? ""
             : selectedBrands?.value || "",
         rentalPrice: selectedPrice?.value || "",
-        minMileage: minMileage || "",
-        maxMileage: maxMileage || "",
+        minMileage: cleanNumber(minMileage) || "",
+        maxMileage: cleanNumber(maxMileage) || "",
       })
     );
 
@@ -59,6 +76,49 @@ export default function SearchBar() {
     setMaxMileage("");
   };
 
+  const customSelectStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: "204px",
+      borderRadius: "12px",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      border: "1px solid #f7f7f7",
+      borderRadius: "12px",
+      width: "204px",
+      height: "272px",
+      boxShadow: "0 4px 36px 0 rgba(0, 0, 0, 0.02)",
+      background: "#fff",
+    }),
+    menuList: (provided) => ({
+      ...provided,
+      maxHeight: "272px",
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      fontFamily: `"Manrope", sans-serif`,
+      fontWeight: 500,
+      fontSize: "16px",
+      lineHeight: "125%",
+      color: state.isSelected ? "#000000" : "#8d929a",
+      backgroundColor: state.isSelected
+        ? "#fff"
+        : state.isFocused
+        ? "#f5f5f5"
+        : "#fff",
+      cursor: "pointer",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "#000000",
+      fontFamily: `"Manrope", sans-serif`,
+      fontWeight: 500,
+      fontSize: "16px",
+      lineHeight: "125%",
+    }),
+  };
+
   return (
     <form className={css.searchBar} onSubmit={handleSubmit}>
       <div className={css.field}>
@@ -66,10 +126,12 @@ export default function SearchBar() {
         <Select
           id="brand"
           options={brandsOptions}
+          components={{ DropdownIndicator }}
           value={selectedBrands}
           onChange={setSelectedBrands}
           placeholder="Choose a brand"
           classNamePrefix="react-select"
+          styles={customSelectStyles}
         />
       </div>
       <div className={css.field}>
@@ -77,10 +139,12 @@ export default function SearchBar() {
         <Select
           id="price"
           options={priceOptions}
+          components={{ DropdownIndicator }}
           value={selectedPrice}
           onChange={setSelectedPrice}
           placeholder="Choose a price"
           classNamePrefix="react-select"
+          styles={customSelectStyles}
           getOptionLabel={(option) => option.label}
           formatOptionLabel={(option, { context }) =>
             context === "value" ? `To $${option.label}` : option.label
@@ -91,15 +155,19 @@ export default function SearchBar() {
         <label>Car mileage / km</label>
         <div className={css.mileage}>
           <input
-            type="number"
+            type="text"
             value={minMileage}
-            onChange={(event) => setMinMileage(event.target.value)}
+            onChange={(event) =>
+              setMinMileage(formatNumber(event.target.value))
+            }
             placeholder="From"
           />
           <input
-            type="number"
+            type="text"
             value={maxMileage}
-            onChange={(event) => setMaxMileage(event.target.value)}
+            onChange={(event) =>
+              setMaxMileage(formatNumber(event.target.value))
+            }
             placeholder="To"
           />
         </div>
